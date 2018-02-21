4f47eac5ec9b4dc1dcdae7bb9df1a63aa3f866a9
Nefelijm <sistemasnik20@gmail.com>
2018-02-21 06:37:39 -0500

    consoleo de precio y estimacion

--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,66 @@
-//Inicializando el mapa y creando
-function initMap(){
+// Inicializando el mapa y creando
+function initMap() {
+  let markador, latitude, longitude;
+  let lima = { lat: -12.1191427,
+lng: -77.0349046};
+  let map = new google.maps.Map(document.getElementById('map'),
+    { 
+      zoom: 18,
+      center: lima
+    });
 
-	let markador , latitude, longitude;
-	let lima = { lat: -12.1191427, lng: -77.0349046};
-	let map = new google.maps.Map(document.getElementById('map'),
-      { 
-				zoom:18,
-				center:lima
-			})
-
-let myUbication = function (position) {
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-	// Agregando marcador (la propiedad position define la posicion de marcador)
+  let myUbication = function(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    // Agregando marcador (la propiedad position define la posicion de marcador)
 	 markador = new google.maps.Marker({		
-		position: lima,
-		map: map,
-	});
+      position: lima,
+      map: map,
+    });
 
-	map.setZoom(20);// Acercamos al mapa
-	map.setCenter(lima);// Asignamos un nuevo centro del mapa
-};
-// Si encontramos algun problema se activa la funcion error
-let error = function (error) {
-	window.alert('No se ha encontrado tu localizacion localización');
-};
+    map.setZoom(20);// Acercamos al mapa
+    map.setCenter(lima);// Asignamos un nuevo centro del mapa
+  };
+  // Si encontramos algun problema se activa la funcion error
+  let error = function(error) {
+    window.alert('No se ha encontrado tu localizacion localización');
+  };
 
-function search() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(myUbication, error);
-	}
-}
-	// llamando a los elementos del  DOM
-	let start = document.getElementById('inputStart');
-	let destination = document.getElementById('inputDestiny');
-	// Autocompletando
-	new google.maps.places.Autocomplete(start);
-	new google.maps.places.Autocomplete(destination);
-	let directionsService = new google.maps.DirectionsService;
-	let directionsDisplay = new google.maps.DirectionsRenderer;
+  function search() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(myUbication, error);
+    }
+  }
+  // llamando a los elementos del  DOM
+  let start = document.getElementById('inputStart');
+  let destination = document.getElementById('inputDestiny');
+  // Autocompletando
+  new google.maps.places.Autocomplete(start);
+  new google.maps.places.Autocomplete(destination);
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
 
-	let calculateRoute = function (directionsService, directionsDisplay){
-		directionsService.route({
-			origin: start.value,
-			destination: destination.value,
-			travelMode:'DRIVING'},function(response,status){
-				if (status === 'OK') {
-					directionsDisplay.setDirections(response);					
-				} else {
-					window.alert('No encontramos tu ruta');
-				}		
-		});
-		
+  let calculateRoute = function(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: start.value,
+      destination: destination.value,
+      travelMode: 'DRIVING'}, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);	
+				console.log(response.routes[0].legs[0]);
+				console.log(response.routes[0].legs[0].distance.text);
+				console.log(response.routes[0].legs[0].duration.text);
 
-	}
+      } else {
+        window.alert('No encontramos tu ruta');
+      }		
+    });
+  };
 
-	directionsDisplay.setMap(map);
+  directionsDisplay.setMap(map);
 
   window.addEventListener('load', search);
-	document.getElementById('rute').addEventListener('click', function () {
-		calculateRoute(directionsService, directionsDisplay);
-
-
-
-		
-		
-	});
-
+  document.getElementById('rute').addEventListener('click', function() {
+    calculateRoute(directionsService, directionsDisplay);
+  });
 };
