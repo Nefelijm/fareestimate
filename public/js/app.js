bdd8689043031f665c76feb2c024ede50ba21596
Nefelijm <sistemasnik20@gmail.com>
2018-02-21 05:08:42 -0500

    Agregando data

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 //Inicializando el mapa y creando
 function initMap(){
-	let icon = 'assets/images/car.jpg';
+
 	let markador , latitude, longitude;
 	let lima = { lat: -12.1191427, lng: -77.0349046};
 	let map = new google.maps.Map(document.getElementById('map'),
@@ -9,9 +9,6 @@ function initMap(){
 				center:lima
 			})
 
-
-
-
 let myUbication = function (position) {
 	latitude = position.coords.latitude;
 	longitude = position.coords.longitude;
@@ -19,29 +16,55 @@ let myUbication = function (position) {
 	 markador = new google.maps.Marker({		
 		position: lima,
 		map: map,
-		icon: icon,
-		animation: google.maps.Animation.DROP, // animar un marcador
-		title: 'Usted esta aqui'
 	});
 
-	map.setZoom(18);// Acercamos al mapa
+	map.setZoom(20);// Acercamos al mapa
 	map.setCenter(lima);// Asignamos un nuevo centro del mapa
 };
-
 // Si encontramos algun problema se activa la funcion error
 let error = function (error) {
 	window.alert('No se ha encontrado tu localizacion localización');
 };
-
 
 function search() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(myUbication, error);
 	}
 }
+	// llamando a los elementos del  DOM
+	let start = document.getElementById('inputStart');
+	let destination = document.getElementById('inputDestiny');
+	// Autocompletando
+	new google.maps.places.Autocomplete(start);
+	new google.maps.places.Autocomplete(destination);
+	let directionsService = new google.maps.DirectionsService;
+	let directionsDisplay = new google.maps.DirectionsRenderer;
+
+	let calculateRoute = function (directionsService, directionsDisplay){
+		directionsService.route({
+			origin: start.value,
+			destination: destination.value,
+			travelMode:'DRIVING'},function(response,status){
+				if (status === 'OK') {
+					directionsDisplay.setDirections(response);					
+				} else {
+					window.alert('No encontramos tu ruta');
+				}		
+		});
+		
+
+	}
+
+	directionsDisplay.setMap(map);
+
+  window.addEventListener('load', search);
+	document.getElementById('rute').addEventListener('click', function () {
+		calculateRoute(directionsService, directionsDisplay);
 
 
 
-window.addEventListener('load', search);
+		
+		
+	});
 
 };
